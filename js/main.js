4ccdf7e7dc35202b7c05d64583b8a6d6e99de8da
Antonella Cino <antonella.cino18@gmail.com>
2020-04-03 17:18:36 +0200

    generazione bombe random

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
     var square = '<div class="square"></div>';
+    var numRandom;
 
+    //ciclo x creazione griglia
     for (i = 0; i < 64; i++) {
         $('.grid').append(square);
-        console.log(square);
-
     }
 
+    generateBombs();
+
+    //fuzione x la generazione delle bombe
+    function generateBombs() {
+        for (i = 0; i <= 15; i++) {
+            numRandom = Math.floor(Math.random() * 64) + 1;
+            if (numRandom === numRandom) {
+                numRandom = Math.floor(Math.random() * 64) + 1;
+            }
+            $('.square').eq(numRandom).addClass('redWannaBe');
+        }
+
+    }
 
 
 
