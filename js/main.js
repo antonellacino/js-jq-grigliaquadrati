795327c2113d11eb4e21856838a16da8eaa04709
Antonella Cino <antonella.cino18@gmail.com>
2020-04-03 16:48:06 +0200

    creazione griglia tramite js/jquery

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var square = '<div class="square"></div>';
+
+    for (i = 0; i < 64; i++) {
+        $('.grid').append(square);
+        console.log(square);
+
+    }
+
+
+
+
+
+
+
+
+
+});
